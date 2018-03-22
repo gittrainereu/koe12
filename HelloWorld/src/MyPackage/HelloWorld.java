f3447aef92db42af8f7a1f3188079fb374e9488a
Jochen Metzger <j.metzger@t3company.de>
2018-03-22 15:38:33 +0100

    done

--- HelloWorld/src/MyPackage/HelloWorld.java
+++ HelloWorld/src/MyPackage/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("feature-4711 change - sysout strg leertaste");
+		System.out.println("master sdf change - sysout strg leertaste");
 
 	}
 
